2232fa16e7442cdc308aa1fe9e966cefad452262
tazkia-33 <ardintatazkia@gmail.com>
2026-05-12 23:02:38 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,27 +1,12 @@
-document.getElementById("loginForm").addEventListener("submit", async function(e) {
+document.getElementById("loginForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
 
-    const res = await fetch("https://herisusanta.my.id/javalogin/api/auth.php", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: `action=login&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
-    });
-
-    const data = await res.json();
-
-    if (data.status === "success") {
-        // simpan username
-            localStorage.setItem("username", data.username);
-            window.location.href = "../index.html";
-         
-    // } else {
-    //     document.getElementById("message").innerText = "Username / Password salah";alert("Login gagal");
-    // }
+    if(username === "admin" && password === "123") {
+        localStorage.setItem("username", username);
+        window.location.href = "../index.html";
     
     } else {
     const alertBox = document.getElementById("alertBox");
