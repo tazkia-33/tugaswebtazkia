7bed4d0757e02134e3c3f088c29887d103f74685
tazkia-33 <ardintatazkia@gmail.com>
2026-05-19 20:43:37 +0700

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,36 +1,44 @@
 document.getElementById("loginForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const savedUser = localStorage.getItem("username");
+    const savedPass = localStorage.getItem("password");
 
-    const res = await fetch("https://herisusanta.my.id/javalogin/api/auth.php", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: `action=login&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
-    });
+    if(username === savedUser &&
+       password === savedPass){
 
-    const data = await res.json();
+        alert("Login berhasil!");
 
-    if (data.status === "success") {
-        // simpan username
-            localStorage.setItem("username", data.username);
-            window.location.href = "../index.html";
-         
-    // } else {
-    //     document.getElementById("message").innerText = "Username / Password salah";alert("Login gagal");
-    // }
+        localStorage.setItem("isLoggedIn", "true");
+
+        window.location.href = "../index.html";
 
     } else {
-    const alertBox = document.getElementById("alertBox");
-    alertBox.innerText = "Username atau Password salah, silahkan coba lagi";
-    alertBox.style.display = "block";
 
-    setTimeout(() => {
-        alertBox.style.display = "none";
-    }, 3000);
-} 
+        alert("Username atau password salah!");
+
+    }
+ const savedUser =
+      localStorage.getItem("username");
+
+    const savedPass =
+      localStorage.getItem("password");
+
+    if(username === savedUser &&
+       password === savedPass){
+
+        alert("Login berhasil!");
+
+        localStorage.setItem("isLoggedIn", "true");
+
+        window.location.href = "../index.html";
+
+    } else {
+
+        alert("Username atau password salah!");
+
+    }
    
 });
